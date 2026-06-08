8cb615f4b05b6a26d9e64de9f520cd7e9ad5e166
ThienCheese <ngthienchi05@gmail.com>
2026-06-08 23:52:00 +0700

    update swaync

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -30,6 +30,6 @@ hl.on("hyprland.start", function ()
     hl.exec_cmd("waybar")
     hl.exec_cmd("ibus-daemon -drx --xim --panel=disable")
     hl.exec_cmd("swww-daemon")   
-    hl.exec_cmd("swaync")
+    hl.exec_cmd("swaync -c /home/cheese/.config/swaync/config.jsonc")
 end)
 
